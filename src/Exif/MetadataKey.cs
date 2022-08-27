84d9d02d9892f7621a9435eb2418bdee499dcd5f
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-08-26 21:20:03 -0600

    Bump copyright year

--- src/Exif/MetadataKey.cs
+++ src/Exif/MetadataKey.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-mozjpeg, a FileType plugin for Paint.NET
 // that saves JPEG images using the mozjpeg encoder.
 //
-// Copyright (c) 2021 Nicholas Hayes
+// Copyright (c) 2021, 2022 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-mozjpeg, a FileType plugin for Paint.NET
 // that saves JPEG images using the mozjpeg encoder.
 //
-// Copyright (c) 2021 Nicholas Hayes
+// Copyright (c) 2021, 2022 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("MozJpegFileType")]
-[assembly: AssemblyCopyright("Copyright © 2021 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2022 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
